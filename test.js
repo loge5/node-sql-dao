8c66542969791dc54ffbcd3a33cd0e63e33e0567
Rolf Loges <loges@traffics.de>
2019-03-18 17:45:18 +0100

    refactor tableSheme classes, define relations

--- test.js
+++ test.js
@@ -15,9 +15,16 @@ mocha.addFile('lib/validators/NumericValidator.spec')
 // DatabaseConnections
 mocha.addFile('lib/databaseConnections/MySqlDatabaseConnection.spec')
 
+// TableShema
+mocha.addFile('lib/tableShema/Column.spec')
+mocha.addFile('lib/tableShema/TableShema.spec')
+mocha.addFile('lib/tableShema/Relation.spec')
+mocha.addFile('lib/tableShema/RelationBelongsTo.spec')
+mocha.addFile('lib/tableShema/RelationHasMany.spec')
+mocha.addFile('lib/tableShema/RelationHasOne.spec')
+mocha.addFile('lib/tableShema/RelationManyMany.spec')
+
 // Generator
-mocha.addFile('lib/TableColumn.spec')
-mocha.addFile('lib/TableShema.spec')
 mocha.addFile('lib/Generator.spec')
 
 mocha.run()
