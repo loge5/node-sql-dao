84f540d67c06ffaca99b3ec0bc243d9319dad248
Rolf Loges <loges@traffics.de>
2019-07-12 11:38:58 +0200

    export relations and validators

--- index.js
+++ index.js
@@ -4,7 +4,14 @@ module.exports.DatabaseConnection = require('./lib/DatabaseConnection')
 module.exports.WhereClause = require('./lib/WhereClause')
 module.exports.MySqlDatabaseConnection = require('./lib/databaseConnections/MySqlDatabaseConnection')
 module.exports.Validator = require('./lib/Validator')
+module.exports.Relation = require('./lib/relation/Relation')
+module.exports.RelationBelongsTo = require('./lib/relation/RelationBelongsTo')
+module.exports.RelationHasOne = require('./lib/relation/RelationHasOne')
+module.exports.RelationHasMany = require('./lib/relation/RelationHasMany')
+module.exports.RelationManyMany = require('./lib/relation/RelationManyMany')
 module.exports.validators = {
+  LengthValidator: require('./lib/validators/LengthValidator'),
+  NumericValidator: require('./lib/validators/NumericValidator'),
   RequiredValidator: require('./lib/validators/RequiredValidator')
 }
 module.exports.Generator = require('./lib/Generator')
